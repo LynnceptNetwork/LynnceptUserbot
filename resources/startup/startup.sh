a534d4e754413bd565395ee7e1c5898a77f70fa6
Lynncept77 <95460649+Lynncept77@users.noreply.github.com>
2022-02-21 12:20:54 +0530

    Create startup.sh

---
+++ resources/startup/startup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Lynncept - UserBot
+# Copyright (C) 2021 LynnceptNetwork
+# This file is a part of < https://github.com/LynnceptNetwork/LynnceptUserbot/ >
+# PLease read the GNU Affero General Public License in <https://www.github.com/LynnceptNetwork/LynnceptUserbot/blob/main/LICENSE/>.
+
+
+echo "
+            ┏━━━━━━━━━━━━
+            Lynncept Userbot  
+            ┗━━━━━━━━━━━━
+
+      Visit @LynnceptNetwork!!
+
+"
+
+python3 -m Lynncept
